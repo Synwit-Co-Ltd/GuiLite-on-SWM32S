9a95b9840d55a1e704b882009e2b84828e09d1d3
woody <whsj215294062@outlook.com>
2021-05-21 18:58:29 +0800

    Add files via upload

---
+++ RTE/_SWM320/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'SWM320_StdPeriph_Driver' 
+ * Target:  'SWM320' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "SWM320.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
